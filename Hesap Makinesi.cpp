17362dc0207113b49396031c6476e2ca1dda0dd1
Busra Aydin <56473075+bbaydn@users.noreply.github.com>
2020-03-31 19:19:09 +0300

    Add files via upload

---
+++ Hesap Makinesi.cpp
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<math.h>
+//Ýlk deneme hesap makinesi
+int main()
+{
+	
+	int islem;
+	int a,b;
+	int sonuc;
+	char name[20];
+	
+	printf("Isminiz nedir?\n");
+	fgets(name, 20, stdin);
+	printf("Merhaba %s su anda cok tatli bir hesap makinesi ile konusuyorsun. \n", name);
+	
+	printf("Yapmak istedigin islemi sayi ile gosterir misin? \n 1- Toplama\n 2- Cikarma\n 3- Carpma\n 4- Bolme\n 5- Us alma\n 6- Karekok\n");
+	scanf("%d", &islem);
+	
+
+	if (islem==1){
+		printf("%s Ilk sayiyiyi yazar misin?\n", name);
+		scanf("%d",&a);
+	
+		printf("%s Ikinci sayiyi yazar misin\n", name);
+		scanf("%d",&b);
+
+	
+		sonuc= a + b;
+		printf("Sonuc= %d", sonuc);
+}
+	else if (islem==2)	{
+		printf("%s Ilk sayiyiyi yazar misin?\n", name);
+		scanf("%d",&a);
+	
+		printf("%s Ikinci sayiyiyi yazar misin?\n", name);
+		scanf("%d",&b);
+		
+		sonuc= a - b;
+		printf("Sonuc= %d", sonuc);
+}
+	else if(islem==3){
+		printf("%s Ilk sayiyiyi yazar misin?\n", name);
+		scanf("%d",&a);
+	
+		printf("%s Ikinci sayiyiyi yazar misin?\n", name);
+		scanf("%d",&b);
+		
+		sonuc= a*b;
+		printf("Sonuc= %d", sonuc);
+}
+	else if(islem==4){
+		printf("%s Ilk sayiyiyi yazar misin?\n", name);
+		scanf("%d",&a);
+	
+		printf("%s Ikinci sayiyiyi yazar misin?\n", name);
+		scanf("%d",&b);
+		
+		sonuc= a/b;
+		printf("Sonuc= %d", sonuc);
+} 
+	else if(islem==5){
+		printf("%s Ilk sayiyiyi yazar misin?\n", name);
+		scanf("%d",&a);
+	
+		printf("%s Ikinci sayiyiyi yazar misin?\n", name);
+		scanf("%d",&b);
+		
+		sonuc= pow(a,b);
+		printf("Sonuc= %d", sonuc);
+}
+	else if(islem==6){
+		printf("%s Kok islemi yapmak istedigin sayiyiyi yazar misin?\n", name);
+		scanf("%d",&a);
+		sonuc =sqrt(a);
+		printf("Sonuc= %d", sonuc);
+	}
+	else
+		printf("%s Hatali giris yaptin. Programi yeniden baslatmalisin.", name);
+	
+	return 0;		
+}
